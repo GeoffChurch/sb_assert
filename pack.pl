970befadf94a3835d4d188c04a5154e2282457ba
Geoffrey Churchill <geoffrey.a.churchill@gmail.com>
2025-01-27 12:18:18 -0500

    Update pack.pl

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(stache).
 version('0.1').
-title('Stash That Actually Can Handle Existentials.').
+title('Stash That Actually Can Handle Existentials').
 author('Geoffrey Churchill', 'geoffrey.a.churchill@gmail.com').
 home('https://github.com/GeoffChurch/stache').
